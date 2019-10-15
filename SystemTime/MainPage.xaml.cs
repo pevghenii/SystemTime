77833ffaefcd8fb24c0549c4847cc648050db2d2
Женя <p_evghenii@yahoo.com>
2019-10-15 11:31:47 +0300

    New

---
+++ SystemTime/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace SystemTime
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        //Timer timer = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            lblText.Text = DateTime.Now.ToString();
+            Thread t1 = new Thread(CheckConnected);
+            t1.Start();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            lblNtpTime.Text = GetNetworkTime().ToString();
+        }
+
+        private void CheckConnected(object obj)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    lblText.Text = DateTime.Now.ToString();
+                });
+            }
+        }
+
+        public static DateTime GetNetworkTime()
+        {
+            const string ntpServer = "pool.ntp.org";
+            var ntpData = new byte[48];
+            ntpData[0] = 0x1B; //LeapIndicator = 0 (no warning), VersionNum = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            socket.Connect(ipEndPoint);
+            socket.Send(ntpData);
+            socket.Receive(ntpData);
+            socket.Close();
+
+            ulong intPart = (ulong)ntpData[40] << 24 | (ulong)ntpData[41] << 16 | (ulong)ntpData[42] << 8 | (ulong)ntpData[43];
+            ulong fractPart = (ulong)ntpData[44] << 24 | (ulong)ntpData[45] << 16 | (ulong)ntpData[46] << 8 | (ulong)ntpData[47];
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+            var networkDateTime = (new DateTime(1900, 1, 1)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime;
+        }
+    }
+}
